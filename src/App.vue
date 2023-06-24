a3f35ab8bf9ff2b0f8a2c31f6c0665c0d66c8be0
Umarillo <114356886+Umarillo@users.noreply.github.com>
2023-06-24 20:11:50 +0500

    test

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,8 @@ export default {
 </script>
 
 <style lang="scss">
-<<<<<<< HEAD
 .main {
   background-color: #fffdf4;
   height: 100vh;
-=======
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
-*{
-  user-select: none;
-}
-.home{
-  display: flex;
->>>>>>> 154fab52d5fa68c3e03054974dc65e9e40bdda58
 }
 </style>
